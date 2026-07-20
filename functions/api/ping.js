548bad379137f652e15e49ed9db001a2cd5308a7
FredrikAnthony <fredrik.anthony@gmail.com>
2026-07-20 16:26:44 +0200

    Use only advanced Pages worker

--- functions/api/ping.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet() {
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: {
-      "content-type": "application/json; charset=utf-8",
-      "cache-control": "no-store"
-    }
-  });
-}
